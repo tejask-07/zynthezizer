5a034663994b2193fd93796e70d9115cda26aaf6
Tejas <tejasdilipkamble@gmail.com>
2026-08-12 00:01:32 +0530

    modified main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
-  std::cout << "welcome to zynthezizer\n";
+  const int screenHeight = 800;
+  const int screenWidth = 650;
+
+  InitWindow(screenWidth, screenHeight, "basic window");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("window hai ji <3", 190, 200, 20, LIGHTGRAY);
+    DrawText("press esc to exit", 190, 240, 20, DARKGRAY);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return 0;
 }
