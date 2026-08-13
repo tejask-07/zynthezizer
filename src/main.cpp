503d0d3cbad2ae8a131a3cb5616b65793795ccfc
Tejas <tejasdilipkamble@gmail.com>
2026-08-13 21:09:37 +0530

    fixed the line connecting issue and added drawing reset

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <cstddef>
 #include <vector>
 
-std::vector<Vector2> points;
+std::vector<std::vector<Vector2>> strokes;
 bool showCenterLine = true;
 
 int main() {
@@ -13,21 +13,30 @@ int main() {
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    if (IsKeyPressed(KEY_R)) {
+      strokes.clear();
+    }
+
     if (IsKeyPressed(KEY_C)) {
       showCenterLine = !showCenterLine;
     }
 
     Vector2 mouse = GetMousePosition();
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-      points.push_back(mouse);
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      strokes.push_back({});
+    }
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !strokes.empty()) {
+      strokes.back().push_back(mouse);
     }
 
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    DrawText("C : toggle center line", 30, screenHeight - 40, 20, GRAY);
+    DrawText("C : toggle center line , R : Clear", 30, screenHeight - 40, 20,
+             GRAY);
 
     if (showCenterLine) {
       DrawLine(0, screenHeight / 2, screenWidth, screenHeight / 2, GRAY);
@@ -36,8 +45,10 @@ int main() {
     DrawText("draw your waveform", 30, 30, 30, BLACK);
     DrawCircle(static_cast<int>(mouse.x), static_cast<int>(mouse.y), 5.0f, RED);
 
-    for (size_t i = 1; i < points.size(); ++i) {
-      DrawLineV(points[i - 1], points[i], BLACK);
+    for (const auto &stroke : strokes) {
+      for (size_t i = 1; i < stroke.size(); ++i) {
+        DrawLineV(stroke[i - 1], stroke[i], BLACK);
+      }
     }
 
     EndDrawing();
